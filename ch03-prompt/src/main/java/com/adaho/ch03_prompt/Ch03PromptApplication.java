55a0eb1ee3c7f62c96f0f1fe431144a903144cfa
lastcoder <aaa@abc.com>
2026-04-15 13:12:56 +0900

    ch03

---
+++ ch03-prompt/src/main/java/com/adaho/ch03_prompt/Ch03PromptApplication.java
@@ -0,0 +1,13 @@
+package com.adaho.ch03_prompt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch03PromptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ch03PromptApplication.class, args);
+	}
+
+}
---
+++ ch03-prompt/src/test/java/com/adaho/ch03_prompt/Ch03PromptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adaho.ch03_prompt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch03PromptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
